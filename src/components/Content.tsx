4182087d596e15de6d892d400128c049145dc8dd
hubeduardo <eduardof.paulo@gmail.com>
2021-09-17 17:16:51 -0300

    desafio 02 - ignite

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,35 @@
-export function Content() {
+import { MovieCard } from '../components/MovieCard';
+
+
+interface MovieProps {
+  movie: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>,
+  genreTitle: string
+}
+
+export function Content(props: MovieProps | undefined) {
   // Complete aqui
+  return(
+    <div className="container">
+        <header>
+          <span className="category">Categoria:<span> {props?.genreTitle}</span></span>
+        </header>
+
+        <main>
+          <div className="movies-list">
+            {props?.movie ? props.movie.map(movie => (
+              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            )) : <div />}
+          </div>
+        </main>
+      </div>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
+import { Button } from '../components/Button';
+
+interface GenreProps {
+  gen: Array<{
+      id: number;
+      title: string;
+      name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    }>,
+  clickMenuItem: Function;   
+  selectedGenreId: number;
+}
+   
+export function SideBar(props: GenreProps | undefined) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {props?.gen ? props.gen.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => props.clickMenuItem(genre.id)}
+              selected={props.selectedGenreId === genre.id}
+            />
+          )) : <div/> }
+        </div>
+
+      </nav>
+  )
 }
